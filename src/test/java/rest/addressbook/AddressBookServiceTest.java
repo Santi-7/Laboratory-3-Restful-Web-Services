9356abb3e8aab6e08d81df189b7cc31cb20bbea3
Santi-7 <santi.gil.begue7@gmail.com>
2015-11-05 23:40:32 +0100

    1 assertion fixed

--- src/test/java/rest/addressbook/AddressBookServiceTest.java
+++ src/test/java/rest/addressbook/AddressBookServiceTest.java
@@ -94,9 +94,12 @@ public class AddressBookServiceTest {
 				.request(MediaType.APPLICATION_JSON)
 				.post(Entity.entity(juan, MediaType.APPLICATION_JSON));
 		List<Person> listBefore=response.readEntity(AddressBook.class).getPersonList();
+		Person pedro = new Person();
+		pedro.setName("Pedro");
+		URI juanURI = URI.create("http://localhost:8282/contacts/person/2");
 		response = client.target("http://localhost:8282/contacts")
 				.request(MediaType.APPLICATION_JSON)
-				.post(Entity.entity(juan, MediaType.APPLICATION_JSON));
+				.post(Entity.entity(pedro, MediaType.APPLICATION_JSON));
 		List<Person> listAfter=response.readEntity(AddressBook.class).getPersonList();
 		assertNotEquals(listBefore.size(), listAfter.size());
 		assertNotEquals(listBefore, listAfter);
