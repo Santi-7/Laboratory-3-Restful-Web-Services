de16e6a06a78e04f19b3e872af73360e22e79327
Santi-7 <santi.gil.begue7@gmail.com>
2015-11-06 22:28:53 +0100

    Update AddressBookService Imports Try 4

--- src/main/java/rest/addressbook/AddressBookService.java
+++ src/main/java/rest/addressbook/AddressBookService.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
