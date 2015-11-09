816f2afd3e9631b405ca1d6ed6152b572a705521
Santi-7 <santi.gil.begue7@gmail.com>
2015-11-09 10:05:58 +0100

    Update error, no imports

--- src/main/java/rest/addressbook/Error.java
+++ src/main/java/rest/addressbook/Error.java
@@ -1,23 +1,17 @@
 package rest.addressbook;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * A managed error
  */
-@ApiModel(value="Error", description="Description of an error ")
+@ApiModel(value="Error", description="Description of an error occured in our service")
 public class Error {
 
 	private int id;
 	private String description;
 
-  public Error (int id, String description) {
-    this.id = id;
-    this.description = description;
-  }
+	public Error (int id, String description) {
+		this.id = id;
+    		this.description = description;
+  	}
+  
 }
