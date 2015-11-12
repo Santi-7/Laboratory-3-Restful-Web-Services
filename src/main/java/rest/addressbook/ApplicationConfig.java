02cd96b5b50a6ce0447ca43165bc7a5220a35085
Iñigo <igascon@outlook.com>
2015-11-12 18:01:09 +0100

    Try temp

--- src/main/java/rest/addressbook/ApplicationConfig.java
+++ src/main/java/rest/addressbook/ApplicationConfig.java
@@ -1,43 +1,54 @@
 package rest.addressbook;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.core.Application;
 import io.swagger.jaxrs.config.BeanConfig;
 
-public class ApplicationConfig extends ResourceConfig {
+public class ApplicationConfig extends Application {
 
-	/**
-     * Default constructor
-     */
-    public ApplicationConfig() {
-    	this(new AddressBook());
-    	BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.2");
-        beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8282");
-        beanConfig.setBasePath("/api");
-        beanConfig.setResourcePackage("rest.addressbook");
-        beanConfig.setScan(true);
-    }
-
-
+	private AddressBook myAddress;
+	
     /**
      * Main constructor
      * @param addressBook a provided address book
      */
     public ApplicationConfig(final AddressBook addressBook) {
-    	register(AddressBookService.class);
-    	register(MOXyJsonProvider.class);
-    	register(new AbstractBinder() {
+    	myAddress = addressBook;
+    	BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.2");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8282");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("rest.addressbook");
+        beanConfig.setScan(true);
+    }
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+
+    	resources.add(AddressBookService.class);
+    	resources.add(MOXyJsonProvider.class);
+    	resources.add(new AbstractBinder() {
 
 			@Override
 			protected void configure() {
-				bind(addressBook).to(AddressBook.class);
+				bind(myAddress).to(AddressBook.class);
 			}
     		
     	});
-    	register(io.swagger.jaxrs.listing.ApiListingResource.class);
-    	register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+    	
+        //resources.add(FirstResource.class);
+        //resources.add(SecondResource.class);
+        //...
+
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        return resources;
     }
 }
